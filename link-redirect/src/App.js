c0349c36ccf040ebeb27ef42524d47a5078c244f
Alex Hetrick <aph1777@rit.edu>
2023-07-07 11:02:05 -0400

    Correcting parameters in emailjs

--- link-redirect/src/App.js
+++ link-redirect/src/App.js
@@ -1,28 +1,28 @@
-import { React, useState, useRef, useEffect } from "react"
+import { React, useState, useRef, useEffect } from "react";
 
-import emailjs from "@emailjs/browser"
-import Logo from "./media/sayyes.jpg"
+import emailjs from "@emailjs/browser";
+import Logo from "./media/sayyes.jpg";
 
 function App() {
-	const [name, setName] = useState("")
-	const [email, setEmail] = useState("")
-	const [redirectLink, setRedirectLink] = useState("")
-	const [isDefault, setIsDefault] = useState(false)
-	const [jobTitle, setJobTitle] = useState("")
-	const [company, setCompany] = useState("")
-	const [jobLink, setJobLink] = useState("")
-	const [jobDescription, setJobDescription] = useState("")
+	const [name, setName] = useState("");
+	const [email, setEmail] = useState("");
+	const [redirectLink, setRedirectLink] = useState("");
+	const [isDefault, setIsDefault] = useState(false);
+	const [jobTitle, setJobTitle] = useState("");
+	const [company, setCompany] = useState("");
+	const [jobLink, setJobLink] = useState("");
+	const [jobDescription, setJobDescription] = useState("");
 
 	useEffect(() => {
-		const queryParameters = new URLSearchParams(window.location.search)
-		const jobid = queryParameters.get("jobid")
+		const queryParameters = new URLSearchParams(window.location.search);
+		const jobid = queryParameters.get("jobid");
 		if (!jobid) {
-			setIsDefault(false)
+			setIsDefault(false);
 		} else {
-			setIsDefault(true)
+			setIsDefault(true);
 			//Get job id from param
 
-			console.log(jobid)
+			console.log(jobid);
 			fetch(
 				`https://say-yes-buffalo-backend.onrender.com/job/${jobid}}`,
 				{
@@ -31,62 +31,62 @@ function App() {
 			)
 				.then((res) => res.json())
 				.then((data) => {
-					console.log(data)
-					setJobTitle(data.title)
-					setJobDescription(data.description)
-					setCompany(data.company)
-					setJobLink(data.joburl)
-				})
+					console.log(data);
+					setJobTitle(data.title);
+					setJobDescription(data.description);
+					setCompany(data.company);
+					setJobLink(data.joburl);
+				});
 		}
-	}, [])
-	const formRef = useRef(null)
-	console.log(jobLink)
+	}, []);
+	const formRef = useRef(null);
+	console.log(jobLink);
 	const submit = () => {
-		let emailPush = sendEmail()
-		let serverPost = postInteraction()
+		let emailPush = sendEmail();
+		let serverPost = postInteraction();
 		Promise.all([emailPush, serverPost]).then(() => {
-			redirect()
-		})
-	}
+			redirect();
+		});
+	};
 
 	const sendEmail = () => {
 		const emailData = {
 			to_name: "Alex",
-		}
+		};
 
-		const formData = Object.fromEntries(new FormData(formRef.current))
-		Object.assign(emailData, formData)
+		const formData = Object.fromEntries(new FormData(formRef.current));
+		Object.assign(emailData, formData);
 
 		let keys = {
 			service_id: "service_2oogo1g",
 			template_id: "template_0er0e33",
 			public_key: "JAQb_etGQpqPy_iDJ",
-		}
+		};
 
 		let params = {
 			to_name: name,
 			email: email,
-			position: "TEMP POSITION",
-			company: "TEMP COMPANY",
-		}
+			position: jobTitle,
+			company: company["name"],
+		};
 
-		console.log("sending email")
+		console.log("sending email");
 		return emailjs
 			.send(keys.service_id, keys.template_id, params, keys.public_key)
 			.then(
 				(result) => {
-					console.log("email sent!")
-					formRef.current.reset()
-					return result
+					console.log("email sent!");
+					formRef.current.reset();
+					return result;
 				},
 				(error) => {
-					alert("An error occurred, Please try again", error.text)
+					alert("An error occurred, Please try again", error.text);
 				}
-			)
-	}
+			);
+	};
 
 	const postInteraction = () => {
-		console.log("posting")
+		console.log("posting");
 
 		return fetch(
 			"https://say-yes-buffalo-backend.onrender.com/interaction",
@@ -101,29 +101,29 @@ function App() {
 			}
 		)
 			.then((response) => {
-				console.log(response)
+				console.log(response);
 				if (response.ok) {
-					console.log("post response ok")
+					console.log("post response ok");
 				} else {
 					console.log(
 						"HTTP error:" +
 							response.status +
 							":" +
 							response.statusText
-					)
-					return null
+					);
+					return null;
 				}
 			})
 			.then(() => {
-				console.log("Succeeded!\nName: " + name + "\nEmail: " + email)
-			})
-	}
+				console.log("Succeeded!\nName: " + name + "\nEmail: " + email);
+			});
+	};
 
 	const redirect = () => {
-		console.log("redirecting")
-		setRedirectLink("https://sayyesbuffalo.org/")
-		window.location.replace(redirectLink)
-	}
+		console.log("redirecting");
+		setRedirectLink(jobLink);
+		window.location = redirectLink;
+	};
 
 	return (
 		<div className="absolute inset-16 rounded-3xl p-8 bg-gray-300 drop-shadow-lg ">
@@ -207,7 +207,7 @@ function App() {
 			)}
 			<hr />
 		</div>
-	)
+	);
 }
 
-export default App
+export default App;
